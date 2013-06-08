b8ac50c6141d3a0b79633b45eb8d890cd6c9bef9
Missaoui Chedy <missaoui.chedy@gmail.com>
2013-06-08 03:02:03 +0100

    Formatting...

--- src/SmartDev/MainBundle/Controller/GeneralDisplayController.php
+++ src/SmartDev/MainBundle/Controller/GeneralDisplayController.php
@@ -5,39 +5,38 @@ use SmartDev\MainBundle\BuisnessLayer\ComponentDTO;
 use SmartDev\MainBundle\BuisnessLayer\ProductDTO;
 class GeneralDisplayController extends Controller {
 	public function displayComponentsAction() {
-		$allComponentsEntities = $this->getDoctrine()->getEntityManager()->getRepository("SmartDevMainBundle:Composant")->findAll();
-		
-        $allComponents = array();
-        foreach ($allComponentsEntities as $entity){
-            $allComponents[] = new ComponentDTO( $entity->getTitre(),
-            $entity->getDescription(),
-            $entity->getlicence()->getNomLicence(),
-            $entity->getNatureComposant()->getNomNature(),
-            $entity->getTypeComposant()->getNomType(),
-            $entity->getEtat()->getNomEtat(),
-            $entity->getVisibilite());	
-           
-        }
+		$allComponentsEntities = $this->getDoctrine()->getEntityManager()
+				->getRepository("SmartDevMainBundle:Composant")->findAll();
+
+		$allComponents = array();
+		foreach ($allComponentsEntities as $entity) {
+			$allComponents[] = new ComponentDTO($entity->getTitre(),
+					$entity->getDescription(),
+					$entity->getlicence()->getNomLicence(),
+					$entity->getNatureComposant()->getNomNature(),
+					$entity->getTypeComposant()->getNomType(),
+					$entity->getEtat()->getNomEtat(), $entity->getVisibilite());
+
+		}
 		$tableModel = new ComponentTableModel($allComponents);
 		return $this
 				->render(
 						"SmartDevMainBundle:GeneralDisplay:components.html.twig",
 						array("model" => $tableModel));
 	}
-	
+
 	public function displayProductsAction() {
-		$allProductsEntities = $this->getDoctrine()->getEntityManager()->getRepository("SmartDevMainBundle:Produit")->findAll();
+		$allProductsEntities = $this->getDoctrine()->getEntityManager()
+				->getRepository("SmartDevMainBundle:Produit")->findAll();
 		$allProducts = array();
-		foreach ($allProductsEntities as $entity){
-			$allProducts[] = new ProductDTO($entity->getTitre(),
- null,
- $entity->getDescription(),
- $entity->getNatureProduit()->getNomNature(),
- $entity->getTypeProduit()->getNomType(),
- $entity->getLicence()->getNomLicence(),
- $entity->getEtat()->getNomEtat(),
- $entity->getVisibilite(),
- null);
+		foreach ($allProductsEntities as $entity) {
+			$allProducts[] = new ProductDTO($entity->getTitre(), null,
+					$entity->getDescription(),
+					$entity->getNatureProduit()->getNomNature(),
+					$entity->getTypeProduit()->getNomType(),
+					$entity->getLicence()->getNomLicence(),
+					$entity->getEtat()->getNomEtat(), $entity->getVisibilite(),
+					null);
 		}
 		$tableModel = new ProductTableModel($allProducts);
 		return $this
